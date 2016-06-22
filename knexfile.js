d38613c7376ffabc3f2eecf8991ffe3922f55d11
Michael Puncel <mpuncel@squareup.com>
2016-06-21 21:45:19 -0700

    add migrations and knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,47 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      database: 'fridgely_development',
+      user:     'fridgely',
+      password: 'poop'
+    }
+  },
+
+  staging: {
+    client: 'mysql',
+    connection: {
+      host: "fridgely-1.ctdccxw5jbvt.us-west-1.rds.amazonaws.com",
+      database: 'fridgely_staging',
+      user:     'fridgely',
+      password: 'uUP8/NMVYqy1X7/Dyqpe78Anni2fAse4pWSsKk6wDl4=' // don't worry, this is just a for-fun app :)
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
---
+++ migrations/20160615223914_items_tables.js
@@ -0,0 +1,34 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTableIfNotExists('purchased_items', function(table) {
+      table.increments();
+      table.integer('item_id').notNullable();
+      table.integer('quantity').notNullable();
+      table.string('unit').notNullable();
+      table.decimal('cost').notNullable();
+      table.timestamp('purchased_at').defaultTo(knex.fn.now());
+    }),
+    knex.schema.createTableIfNotExists('used_items', function(table) {
+      table.increments();
+      table.integer('item_id').notNullable();
+      table.integer('quantity').notNullable();
+      table.string('unit').notNullable();
+      table.decimal('cost').notNullable();
+      table.timestamp('used_at').defaultTo(knex.fn.now());
+    }),
+    knex.schema.createTableIfNotExists('items', function(table) {
+      table.increments();
+      table.string('name').notNullable();
+      table.integer('quantity').notNullable();
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('items'),
+    knex.schema.dropTable('purchased_items'),
+    knex.schema.dropTable('used_items')
+  ]);
+};

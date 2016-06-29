9139ce9f2ba2da7f7d2d9ec95a1152cb88e8ae2e
Mari Miyachi <mari.miyachi@opower.com>
2016-06-28 21:26:58 -0700

    POST accepts list of items
    POST and DELETE now use promises to return 200 responses only when database transactions are successful.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express = require('express'),
 	UsedItem = require('./database/used_item');
 
 var app = express();
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -29,33 +29,37 @@ app.use(function(req, res, next) {
 app.post('/items', function (req, res) {
 	if (!req.body) return res.sendStatus(400);
 
-	var item = req.body;
-
-	Item.where('name', item.name).fetch().then(function(results){
-		if (results === null) {
-			Item.forge({name: item.name, quantity: item.quantity}).save().then(function(r) {
-				var itemId = r.get('id');
-				PurchasedItem.forge({item_id: itemId, quantity: item.quantity, unit: 0, cost: 0}).save().then(function(r) {
-					if (r) console.log('POST add item');
+	var itemList = req.body;
+	promises = itemList.map(function(item) {
+		Item.where('name', item.name).fetch().then(function(results){
+			if (results === null) {
+				Item.forge({name: item.name, quantity: item.quantity}).save().then(function(r) {
+					var itemId = r.get('id');
+					PurchasedItem.forge({item_id: itemId, quantity: item.quantity, unit: 0, cost: 0}).save().then(function(r) {
+						if (r) console.log('post add item');
+					});
 				});
-			});
-		} else {
-			Item.forge().where({name: item.name}).save({quantity: parseInt(results.get('quantity'))+parseInt(item.quantity)}, { method: 'update' }).then(function(r){
-				var itemId = results.get('id');
-				PurchasedItem.forge({item_id: itemId, quantity: item.quantity, unit: 0, cost: 0}).save().then(function(r) {
-					if (r) console.log('POST add item');
+			} else {
+				Item.forge().where({name: item.name}).save({quantity: parseInt(results.get('quantity'))+parseInt(item.quantity)}, { method: 'update' }).then(function(r){
+					var itemId = results.get('id');
+					PurchasedItem.forge({item_id: itemId, quantity: item.quantity, unit: 0, cost: 0}).save().then(function(r) {
+						if (r) console.log('POST add item');
+					});
 				});
-			});
-		}
+			}
+		});
+	});
+	Promise.all(promises).then(function() {
+		return res.send('Success');
 	});
 });
 
 // request should be a list with each element having a name, quantity, and unit
-app.delete('/removeItems', function (req, res) {
+app.delete('/items', function (req, res) {
 	if (!req.body) return res.sendStatus(400);
 
 	var itemList = req.body;
-	itemList.forEach(function(item) {
+	promises = itemList.map(function(item) {
 		Item.where('name', item.name).fetch().then(function(results){
 			if (results === null) {
 				// error since any removed item should already exist
@@ -68,7 +72,10 @@ app.delete('/removeItems', function (req, res) {
 				if (results) return res.end('DELETE remove item');
 			});
 		});
-	})
+	});
+	Promise.all(promises).then(function() {
+		return res.send('Success');
+	});
 });
 
 app.get('/items', function(req, res) {
